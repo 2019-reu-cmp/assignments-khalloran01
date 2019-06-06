fb93ff1c3b24cd4562b6ab1aa00f76533bde35f5
Kathleen Halloran <khallor1@nd.edu>
2019-06-06 16:35:19 -0400

    Day4

--- Day03/pi.py
+++ Day03/pi.py
@@ -4,9 +4,16 @@
 import math
 
 def srinivasa(k):
-    """
-    """
-
+    i=0
+    calc=0
+    con=(2*(math.sqrt(2))/9801)
+    while i<k:
+        calc+=(math.factorial(4*i)*(1103+26390*i))/(((math.factorial(i))**4)*(396**(4*i)))
+        print("i", i)
+        i+=1 
+    total= 1/(con*calc)
+    return total, total-(math.pi)
+    
 if __name__ == '__main__':
-    iterations = 1
+    iterations = 2
     print(srinivasa(iterations))
